410a509056a3c0de6bbf2f1cc493550d1d2447df
Riyadulislamratul <riadul1810833319@gmail.com>
2026-01-16 19:52:16 +0600

    Mini Cards Added

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -3,6 +3,11 @@ import './App.css'
 import Search from "./components/Search.jsx"
 import Banner from "./components/Banner.jsx"
 import Sale_card from "./components/Sale_card.jsx"
+import Mini_card from "./components/Mini_card.jsx"
+import truck from "./assets/truck.png"
+import payment from "./assets/payment.png"
+import call from "./assets/call.png"
+import gift from "./assets/gift.png"
 
 const App = () => {
   return (
@@ -16,8 +21,13 @@ const App = () => {
           <Sale_card  para="SALE UP TO 25%" heading="Make a Statement" />
         </div>
       </section>
-      <section>
-        
+      <section className="container">
+        <div className="mini_cards">
+          <Mini_card img={truck} heading="Free Shipping" para="For all Orders Over $100" />
+          <Mini_card img={payment} heading="Secured Payment" para="Payment Cards Accepted" />
+          <Mini_card img={call} heading="30 Days Returns" para="For an Exchange Product" />
+          <Mini_card img={gift} heading="24/7 Support" para="Contact us Anytime" />
+        </div>
       </section>
     </>
   )
--- my-app/src/components/Mini_card.jsx
+++ my-app/src/components/Mini_card.jsx
@@ -4,6 +4,9 @@ const Mini_card = (props) => {
   return (
     <div className="mini_card">
             <div className="mini_content">
+                <div className="mini_image">
+                  <img src={props.img} alt="" />
+                </div>
                 <h4>{props.heading}</h4>
                 <p>{props.para}</p>
             </div>
